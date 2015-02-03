4cdcb96d1b1be27da2561bef85b6a38509268c85
pvelez <pablovv2012@gmail.com>
2015-02-03 20:40:51 +0000

    First commit. All this code and structure have been generated using yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name admiFacilApp
+ * @description
+ * # admiFacilApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('admiFacilApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
